eac2783ff0977424ca7b15ffd36bd20bb2e0f498
ShwoTimeNow <437838009@qq.com>
2015-11-06 15:30:40 +0800

    message 是啥

--- src/src/main/java/com/example/zwl/src/MainActivity.java
+++ src/src/main/java/com/example/zwl/src/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	//你好，这是个好的开始！
         setContentView(R.layout.activity_main);
     }
 
